66326c05a40dfac13218ed5c5eceaf4d74e80968
ChoiJinhan <choijinhan@ChoiJinhanui-MacBook-Pro-2.local>
2017-05-23 17:01:22 +0900

    add test code for get topics and create new topic

--- home.py
+++ home.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
+from kafka import KafkaConsumer, KafkaClient
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def main():
      return render_template("main.html")
 
+@app.route('/spots',methods=['GET'])
+def spots():
+    topics = list(consumer.topics())
+    topics.remove("__consumer_offsets")
+    print(topics)
+    print(consumer)
+    return render_template("spots.html",topics = topics)
+
+@app.route('/create')
+def createTopic():
+    client = KafkaClient(hosts=['0.0.0.0:9092'])
+    client.ensure_topic_exists('newnew_topic')
+    return render_template("spots.html")
 if __name__ == '__main__':
+    consumer = KafkaConsumer(bootstrap_servers=['0.0.0.0:9092'])
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8088)
 
