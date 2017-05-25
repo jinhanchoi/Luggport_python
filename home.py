341c985a495501f8689d52b2e1c5eaa77191dd3a
ChoiJinhan <choijinhan@ChoiJinhanui-MacBook-Pro-2.local>
2017-05-25 21:37:48 +0900

    edit request param function

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from kafka import KafkaConsumer, KafkaClient
 import mongoAccess
 
@@ -10,19 +10,30 @@ def main():
 
 @app.route('/spots',methods=['GET'])
 def spots():
-    # topics = list(consumer.topics())
-    # topics.remove("__consumer_offsets")
-    # print(topics)
-    # print(consumer)
+    topics = list(consumer.topics())
+    #topics.remove("__consumer_offsets")
+    print(topics)
+    print(consumer)
     spots = dao.db['spot']
     for tspot in spots.find():
         print(tspot)
     return render_template("spots.html",spots = spots.find())
 
-@app.route('/create')
+@app.route('/create',methods=['POST'])
 def createTopic():
-    client = KafkaClient(hosts=['0.0.0.0:9092'])
-    client.ensure_topic_exists('newnew_topic')
+    
+    name = request.form.get('name')
+    location = request.form.get('location')
+    # client = KafkaClient(hosts=['0.0.0.0:9092'])
+    client.ensure_topic_exists(name)
+   
+    spot = dao.db['spot']
+    if spot.find_one({'name' : name}) is None:
+        spot.insert({'name':name,'location':location})
+    return render_template("spots.html")
+@app.route('/deleteTopic')
+def delTopics():
+    
     return render_template("spots.html")
 
 @app.route('/mongo')
@@ -41,7 +52,8 @@ def testMongoRead():
 
 
 if __name__ == '__main__':
-    # consumer = KafkaConsumer(bootstrap_servers=['0.0.0.0:9092'])
+    consumer = KafkaConsumer(bootstrap_servers=['0.0.0.0:9092'])
+    client = KafkaClient(hosts=['0.0.0.0:9092'])
     dao = mongoAccess.MongoAccessObject({'host':'localhost','name':'spots'})
     app.debug = True
     app.run(host='0.0.0.0', port=8088)
